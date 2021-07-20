00f10a38085987ebe6c659d02091cea7f5cf7d50
LEONEL-PNP <83434309+LEONEL-PNP@users.noreply.github.com>
2021-07-19 21:01:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const backgroundbyeimg = `https://i.imgur.com/2TCj9ri.jpg`
 const apikeyimgbb = 'c9cf132756510ad171587fb5a5a0389f'
 // sua key do imgbb para ativar o antiporn e mais alguns comando que necessita de baixar imagem
 
-const cr = '☄️✨𝑩𝑹⃟𝑰𝒁𝑨𝑺 𝑶⃟𝑹𝑰𝑶𝑵✨☄️\n' 
+const cr = '☄️✨ৣ››🐰͢⸸ғᴋ᭭-ʟᷤᴇͤᴏͧɴͪᴇͤʟᷤ⸸✨☄️\n' 
 // texto do simbolo de verificado
 
 const prefix = '!' 
